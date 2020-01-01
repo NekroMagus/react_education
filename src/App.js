29a278ae9b3b5d072201db7aaac7a6164183a58b
NekroMagus <NekroMagus@mail.ru>
2020-01-01 23:57:11 +0300

    breakdown into modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import './bootstrap.min.css';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div class="container-fluid">
+            <div className="row">
+               <Header/>
+               <Navbar/>
+               <Profile/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
