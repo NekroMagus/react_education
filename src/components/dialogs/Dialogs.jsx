ee992fa9489c6dddad6f74bcf6b516ad00bc7ef0
NekroMagus <NekroMagus@mail.ru>
2020-01-04 03:19:17 +0300

    added dialogs 2 rows

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -3,10 +3,24 @@ import css from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <main>
-            Dialogs
+        <main className="row">
+            <div className={`col-md-3 ${css.dialogs}`}>
+                <ul>
+                    <li>Sasha</li>
+                    <li>Vasya</li>
+                    <li>Sveta</li>
+                    <li>Katya</li>
+                </ul>
+            </div>
+            <div className={`col-md-9 ${css.messages}`}>
+                <ul>
+                    <li>Hi</li>
+                    <li>Hello</li>
+                    <li>How are u?</li>
+                </ul>
+            </div>
         </main>
-    )
+    );
 };
 
 export default Dialogs;
