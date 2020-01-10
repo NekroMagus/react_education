08e7b7f21315be1604eb15a578927f7cd8481565
NekroMagus <NekroMagus@mail.ru>
2020-01-10 11:40:54 +0300

    remove unused exports and refactoring

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import './bootstrap.min.css';
 import './App.css';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
-import Profile from "./components/content/profile/Profile";
-import Dialogs from "./components/content/dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
 import Content from "./components/content/Content";
 
 const App = (props) => {
     return (
-        <BrowserRouter>
             <div className="container-fluid">
                 <div className="row">
                     <Header/>
@@ -20,7 +16,6 @@ const App = (props) => {
                              user={props.state.user}/>
                 </div>
             </div>
-        </BrowserRouter>
     );
 };
 
