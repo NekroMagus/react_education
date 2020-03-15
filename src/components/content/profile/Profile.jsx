e9aff4b3e34a860a47cacd836cf0cae4b8a68d06
NekroMagus <NekroMagus@mail.ru>
2020-03-15 08:16:39 +0300

    changed from function to class

--- src/components/content/profile/Profile.jsx
+++ src/components/content/profile/Profile.jsx
@@ -1,29 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import ProfileImage from '../../../profile_image.jpg';
 import css from './Profile.module.css';
-import MyPosts from "./posts/MyPosts";
-import {connect} from "react-redux";
 import MyPostsContainer from "./posts/MyPostsContainer";
 
-const Profile = (props) => {
-    return (
-        <section className="col-md-9">
-            <div className="row">
-                <div className="col-md-4">
-                    <img src={ProfileImage} className={css.img} alt="profile image"/>
+class Profile extends Component {
+    render() {
+        return (
+            <section className="col-md-9">
+                <div className="row">
+                    <div className="col-md-4">
+                        <img src={ProfileImage} className={css.img} alt="profile image"/>
+                    </div>
+                    <div className="col-md-7">
+                        <p>My name is: {this.props.name}</p>
+                        <p>My surname is: {this.props.surname}</p>
+                    </div>
                 </div>
-                <div className="col-md-7">
-                    <p>My name is: {props.name}</p>
-                    <p>My surname is: {props.surname}</p>
+                <div className="row">
+                    <div className="col-md-12">
+                        <MyPostsContainer/>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <MyPostsContainer/>
-                </div>
-            </div>
-        </section>
-    );
-};
+            </section>
+        );
+    }
+}
 
 export default Profile;
